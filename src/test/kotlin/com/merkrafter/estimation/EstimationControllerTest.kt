41bdb1ba5cc6049c8a2fad4963a34cc7c94e9f39
merkrafter <merkrafter@gmail.com>
2021-05-08 20:03:39 +0200

    Add springmockk dependency

--- src/test/kotlin/com/merkrafter/estimation/EstimationControllerTest.kt
+++ src/test/kotlin/com/merkrafter/estimation/EstimationControllerTest.kt
@@ -1,12 +1,42 @@
 package com.merkrafter.estimation
 
+import com.ninjasquad.springmockk.MockkBean
+import io.mockk.every
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
-import org.junit.jupiter.api.Assertions.*
+/**
+ * @author merkrafter
+ */
+@WebMvcTest
+internal class EstimationControllerTest(@Autowired val mockMvc: MockMvc) {
 
-internal class EstimationControllerTest {
+    @MockkBean
+    private lateinit var productRepository: ProductRepository
+
+    private val productsPath = "/products"
 
     @Test
-    fun listProductNames() {
+    fun `GETing 'products' should return all products`() {
+        val book = Product("Book", 2399)
+        val cd = Product("CD", 1499)
+        every { productRepository.findAll() } returns listOf(book, cd)
+        mockMvc.perform(get(productsPath).accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk)
+            .andExpect(jsonPath("\$.[0]").value(book.name))
+            .andExpect(jsonPath("\$.[1]").value(cd.name))
+    }
+
+    @Test
+    fun `GETing 'products' with empty repository should have status code OK`() {
+        every { productRepository.findAll() } returns emptyList()
+        mockMvc.perform(get(productsPath).accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk)
     }
 }
