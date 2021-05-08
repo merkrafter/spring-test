fdd6f343cc2cb2709fef7c5f2491ca03e5d4a312
merkrafter <merkrafter@gmail.com>
2021-05-08 17:41:34 +0200

    Initial commit off Spring initializr

---
+++ src/main/kotlin/com/merkrafter/estimation/EstimationApplication.kt
@@ -0,0 +1,11 @@
+package com.merkrafter.estimation
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EstimationApplication
+
+fun main(args: Array<String>) {
+	runApplication<EstimationApplication>(*args)
+}
---
+++ src/test/kotlin/com/merkrafter/estimation/EstimationApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.merkrafter.estimation
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EstimationApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
